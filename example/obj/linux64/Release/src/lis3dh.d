58eaec2d4c6d71583f6898cf9bf54c2317a0783a
pierrextardif <pierre.tardif00@gmail.com>
2019-07-16 10:49:52 +0100

    obj folder

--- example/obj/linux64/Release/src/lis3dh.d
+++
@@ -1,5 +0,0 @@
-obj/linux64/Release/src/lis3dh.o: \
- /home/pierre/Documents/OF/apps/ofxLedMatrix/example/src/lis3dh.cpp \
- /home/pierre/Documents/OF/apps/ofxLedMatrix/example/src/lis3dh.h
-
-/home/pierre/Documents/OF/apps/ofxLedMatrix/example/src/lis3dh.h:
--- example/obj/linuxarmv6l/Release/src/lis3dh.d
+++
@@ -1,5 +0,0 @@
-obj/linuxarmv6l/Release/src/lis3dh.o: \
- /home/pi/openFrameworks/addons/ofxLedMatrix/example/src/lis3dh.cpp \
- /home/pi/openFrameworks/addons/ofxLedMatrix/example/src/lis3dh.h
-
-/home/pi/openFrameworks/addons/ofxLedMatrix/example/src/lis3dh.h:
